497b7dae423201c30f60bc40ba58a27b78f0fd85
Jaime Visser <jaimevisser@gmail.com>
2013-05-10 17:33:36 +0200

    Changed header style

--- header.php
+++ header.php
@@ -11,8 +11,7 @@
     <div id="super">
     <header role="banner">
         <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name','display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-        <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<?php wp_nav_menu( array( 'theme_location' => 'header', 'menu_class' => 'nav-menu' ) ); ?>
+<?php wp_nav_menu( array( 'theme_location' => 'header', 'container' => false ,'menu_class' => 'nav-menu' ) ); ?>
 		</nav>
 	</header>
