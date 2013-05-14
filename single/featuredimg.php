b4ef31e7ae3912e2288c7b9e9ebff49069616f52
Jaime Visser <jaimevisser@gmail.com>
2013-05-14 21:28:07 +0200

    Leave out image section when no featured image is present

--- single/featuredimg.php
+++ single/featuredimg.php
@@ -7,8 +7,10 @@ $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID)
     <?php the_title(); ?>
     </h2>
 
+    <?php if(has_post_thumbnail()){ ?>
     <div class="featuredimage" style="background-image:url('<?php echo $large_image_url[0]; ?>');">
     </div>
+    <?php } ?>
 
     <div class="content">
         <?php the_content(); ?>
